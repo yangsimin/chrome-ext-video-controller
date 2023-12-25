7e23f4db5845096c9c3afad8342e10c2e54be2fa
yangsimin <yangsimin@users.noreply.github.com>
2023-12-25 13:15:21 +0800

    重构代码；格式化代码

--- src/contentScripts/views/App.vue
+++ src/contentScripts/views/App.vue
@@ -1,4 +1,15 @@
 <script setup lang="ts">
+/**
+ * h/H: 后退
+ * l/L: 前进
+ * j: 减小音量
+ * k: 增加音量
+ * m: 静音 / 取消静音
+ * z: 重置播放倍速
+ * x: 减小播放倍速
+ * c: 增加播放倍速
+ *  (space): 暂停
+ */
 import type { UseStyleTagReturn } from '@vueuse/core'
 import { useStyleTag } from '@vueuse/core'
 import 'uno.css'
@@ -12,135 +23,116 @@ const videoSelectedIndex = ref(0)
 
 const videoListRef = ref(null)
 
-videoController()
+let toast: Toast | undefined
+let video: HTMLVideoElement
 
-function videoController() {
-  /**
- * h/H: 后退
- * l/L: 前进
- * j: 减小音量
- * k: 增加音量
- * m: 静音 / 取消静音
- * z: 重置播放倍速
- * x: 减小播放倍速
- * c: 增加播放倍速
- *  (space): 暂停
- */
+window.addEventListener('load', () => {
+  init()
+})
 
-  let toast: Toast | undefined
-  let video: HTMLVideoElement
-
-  window.addEventListener('load', () => {
-    mapKey()
-    toast = new Toast()
+function init() {
+  toast = new Toast()
+  observeVideoUpdate(() => {
+    videoElements.value = getAllValidVideoElements()
+    videoSelectedIndex.value = Math.min(
+      videoSelectedIndex.value,
+      videoElements.value.length - 1,
+    )
+    video = videoElements.value[videoSelectedIndex.value]
+    logInfo(videoElements.value)
   })
 
-  function getAllValidVideoElements() {
-    return Array.from(document.querySelectorAll('video') ?? []).filter(video => video.src)
-  }
+  window.addEventListener(
+    'keyup',
+    event => {
+      if (!shouldMapKey(event)) return
 
-  function shouldMapKey(event: KeyboardEvent) {
-    if (['input', 'textarea'].includes((event.target as Element)?.tagName?.toLowerCase()))
-      return false
-
-    if (event.metaKey || event.altKey || event.ctrlKey)
-      return false
-
-    if (!videoElements.value?.length)
-      return false
-
-    return true
-  }
-
-  // 映射按键
-  function mapKey() {
-    const observer = new MutationObserver((mutations) => {
-      mutations.forEach((mutation) => {
-        mutation.addedNodes?.forEach((node) => {
-          if (node.nodeName !== 'VIDEO')
-            return
-
-          setTimeout(() => {
-            videoElements.value = getAllValidVideoElements()
-            logInfo(videoElements.value)
-          }, 1000)
-        })
-        mutation.removedNodes?.forEach((node) => {
-          if (node.nodeName !== 'VIDEO')
-            return
-
-          setTimeout(() => {
-            videoElements.value = getAllValidVideoElements()
-            logInfo(videoElements.value)
-          })
-        })
-      })
-    })
-    observer.observe(document.body, {
-      subtree: true,
-      childList: true,
-    })
-
-    setTimeout(() => {
-      videoElements.value = getAllValidVideoElements()
-      logInfo(videoElements.value)
-    })
-
-    window.addEventListener('hashchange', () => {
-      videoElements.value = getAllValidVideoElements()
-      logInfo(videoElements.value)
-    })
-
-    window.addEventListener('keyup', (event) => {
-      if (!shouldMapKey(event))
-        return
-
-      if (videoSelectedIndex.value >= videoElements.value.length)
-        videoSelectedIndex.value = videoElements.value.length - 1
-
-      video = videoElements.value[videoSelectedIndex.value]
       // 按下空格, 特殊处理
       const exceptKeys = [' ']
       if (exceptKeys.includes(event.key)) {
         event.stopImmediatePropagation()
         event.preventDefault()
       }
-    }, {
+    },
+    {
       capture: true,
-    })
+    },
+  )
 
-    window.addEventListener(
-      'keydown',
-      async (event) => {
-        if (!shouldMapKey(event))
-          return
+  window.addEventListener(
+    'keydown',
+    async event => {
+      if (!shouldMapKey(event)) return
 
-        if (videoSelectedIndex.value >= videoElements.value.length)
-          videoSelectedIndex.value = videoElements.value.length - 1
+      const features = [mapVolume, mapTime, mapSpeed, mapPause, mapScreen]
+      const ret = features.find(func => !!func(event.key, video!, toast!))
 
-        video = videoElements.value[videoSelectedIndex.value]
-        const features = [mapVolume, mapTime, mapSpeed, mapPause, mapScreen]
-        const ret = features.find(func => !!func(event.key, video!, toast!))
-
-        if (ret) {
-          // 避免和网站原本的快捷键功能冲突，优先使用我们自定义的
-          event.stopImmediatePropagation()
-          event.preventDefault()
-        }
-      },
-      {
+      if (ret) {
+        // 避免和网站原本的快捷键功能冲突，优先使用我们自定义的
+        event.stopImmediatePropagation()
+        event.preventDefault()
+      }
+    },
+    {
       // 在事件捕获阶段执行回调
       // 这样可以保证回调的优先级，有利于后续阻止其他 handle 执行
-        capture: true,
-      },
+      capture: true,
+    },
+  )
+}
+
+function observeVideoUpdate(onVideoUpdate: () => void) {
+  const observer = new MutationObserver(mutations => {
+    mutations.forEach(mutation => {
+      mutation.addedNodes?.forEach(node => {
+        if (node.nodeName !== 'VIDEO') return
+
+        setTimeout(onVideoUpdate, 1000)
+      })
+      mutation.removedNodes?.forEach(node => {
+        if (node.nodeName !== 'VIDEO') return
+
+        setTimeout(onVideoUpdate)
+      })
+    })
+  })
+
+  observer.observe(document.body, {
+    subtree: true,
+    childList: true,
+  })
+
+  setTimeout(onVideoUpdate)
+
+  window.addEventListener('hashchange', onVideoUpdate)
+}
+
+function getAllValidVideoElements() {
+  return Array.from(document.querySelectorAll('video') ?? []).filter(
+    video => video.src,
+  )
+}
+
+function shouldMapKey(event: KeyboardEvent) {
+  if (
+    ['input', 'textarea'].includes(
+      (event.target as Element)?.tagName?.toLowerCase(),
     )
-  }
+  )
+    return false
+
+  if (event.metaKey || event.altKey || event.ctrlKey) return false
+
+  if (!videoElements.value?.length) return false
+
+  return true
 }
 
 const videoHeight = ref(0)
 const videoWidth = ref(0)
 let styleTag: UseStyleTagReturn | undefined
-const highlightClass = computed(() => `
+const highlightClass = computed(
+  () => `
   .video-controller-highlight::before {
     content: "";
     display: block;
@@ -155,7 +147,8 @@ const highlightClass = computed(() => `
     width: ${videoWidth.value}px;
     height: ${videoHeight.value}px;
   }
-`)
+`,
+)
 
 function selectVideo(index: number) {
   const video = videoElements.value[index]
@@ -202,16 +195,26 @@ function logInfo(message: any, ...optionalParams: any[]) {
     >
       <li
         v-for="index in videoElements.length"
-        :key="index" @mouseenter="selectVideo(index - 1)" @mouseleave="unselectVideo(index - 1)"
+        :key="index"
+        @mouseenter="selectVideo(index - 1)"
+        @mouseleave="unselectVideo(index - 1)"
       >
-        <label class="flex items-center cursor-pointer py-4px"><input v-model="videoSelectedIndex" type="radio" :value="index - 1">Video {{ index }}</label>
+        <label class="flex items-center cursor-pointer py-4px"
+          ><input
+            v-model="videoSelectedIndex"
+            type="radio"
+            :value="index - 1"
+          />Video {{ index }}</label
+        >
       </li>
     </ul>
     <button
       class="flex w-40px h-40px rounded-full shadow cursor-pointer border-none"
       bg="teal-600 hover:teal-700"
     >
-      <pixelarticons-power class="block m-auto text-white text-18px leading-28px" />
+      <pixelarticons-power
+        class="block m-auto text-white text-18px leading-28px"
+      />
     </button>
   </div>
 </template>
