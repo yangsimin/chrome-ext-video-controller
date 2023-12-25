d70b3e0ea1a07d0c46ac4a4b6107301556e558e6
yangsimin <yangsimin@users.noreply.github.com>
2023-12-25 14:28:01 +0800

    添加类型定义

---
+++ src/contentScripts/types.ts
@@ -0,0 +1,10 @@
+export interface Context {
+  event: KeyboardEvent
+  video: HTMLVideoElement
+  toast: Toast
+}
+
+export interface Plugin {
+  onKeyUp: (context: Context) => boolean
+  onKeyDown: (context: Context) => boolean
+}
